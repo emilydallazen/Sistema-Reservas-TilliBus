33127b67a8a3813435f2478726b2cdb6b73d6b3c
Felipe Daniel Nerling <felipednerling@gmail.com>
2024-06-25 06:04:00 -0300

    Alteração no layout da tela de login - ocultar sidebar

--- src/src/App.js
+++ src/src/App.js
@@ -1,15 +1,14 @@
+// src/App.js
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Box, CssBaseline, Toolbar } from '@mui/material';
 import Dashboard from './components/Dashboard';
 import Users from './components/Users';
 import Settings from './components/Settings';
 import LoginPage from './pages/LoginPage';
 import ProtectedRoute from './components/ProtectedRoute';
-import Navbar from './components/Navbar';
-import Sidebar from './components/Sidebar';
 import MyCalendar from './components/MyCalendar'; 
 import Financeiro from './components/Financeiro'; 
+import MainLayout from './components/MainLayout';
 
 function App() {
   const [isDrawerOpen, setIsDrawerOpen] = useState(true);
@@ -20,46 +19,20 @@ function App() {
 
   return (
     <Router>
-      <CssBaseline />
-      <Navbar toggleDrawer={toggleDrawer} isDrawerOpen={isDrawerOpen} />
-      <Box sx={{ display: 'flex' }}>
-        <Sidebar open={isDrawerOpen} handleDrawerClose={toggleDrawer} />
-        <Box component="main" sx={{ flexGrow: 1, p: 3, marginTop: '64px', marginLeft: isDrawerOpen ? '240px' : '60px', transition: 'margin 0.3s' }}>
-          <Toolbar />
-          <Routes>
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/Agenda" element={
-              <ProtectedRoute>
-                <MyCalendar />
-              </ProtectedRoute>
-            } />
-            
-            <Route path="/Cadastros" element={
-              <ProtectedRoute>
-                <Users />
-              </ProtectedRoute>
-            } />
-            
-            <Route path="/Dashboard" element={
-              <ProtectedRoute> 
-                <Dashboard />
-              </ProtectedRoute>
-            } />
-
-            <Route path="/Config" element={
-              <ProtectedRoute>
-                <Settings />
-              </ProtectedRoute>
-            } />
-
-            <Route path="/Financeiro" element={
-              <ProtectedRoute>
-                <Financeiro />
-              </ProtectedRoute>
-            } />
-          </Routes>
-        </Box>
-      </Box>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={
+          <ProtectedRoute>
+            <MainLayout isDrawerOpen={isDrawerOpen} toggleDrawer={toggleDrawer} />
+          </ProtectedRoute>
+        }>
+          <Route path="Agenda" element={<MyCalendar />} />
+          <Route path="Cadastros" element={<Users />} />
+          <Route path="Dashboard" element={<Dashboard />} />
+          <Route path="Config" element={<Settings />} />
+          <Route path="Financeiro" element={<Financeiro />} />
+        </Route>
+      </Routes>
     </Router>
   );
 }
